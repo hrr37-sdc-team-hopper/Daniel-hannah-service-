39ee8fc18cd9b4956cae389937aaeb64e243ffc7
Hannah Kim <hannahsohkim@gmail.com>
2019-04-01 23:16:18 -0700

    Complete post route to add review

--- server/app.js
+++ server/app.js
@@ -11,8 +11,8 @@ app.use('/books/:id', express.static(__dirname + '/../client/dist'))
 
 // get all reviews
 app.get('/books/:id/reviews', (req, res, next) => {
-  const id = req.params.id;
-  db.getReviews(id).then((reviews) => {
+  let id = req.params.id;
+  db.getReviews(id).then(reviews => {
     res.send(reviews)
   })
   .catch(next)
@@ -20,11 +20,11 @@ app.get('/books/:id/reviews', (req, res, next) => {
 
 // get reviews w/ specific rating
 app.get('books/:id/reviews/:rating', (req, res, next) => {
-  const id = req.params.id;
-  const rating = req.params.rating;
+  let id = req.params.id;
+  let rating = req.params.rating;
   console.log(id, 'IDDDD')
   console.log(rating, 'RATINGGGGG')
-  db.getRatedReviews(id, rating).then((ratedReviews) => {
+  db.getRatedReviews(id, rating).then(ratedReviews => {
     res.send(ratedReviews)
   })
   .catch(next);
@@ -32,10 +32,14 @@ app.get('books/:id/reviews/:rating', (req, res, next) => {
 
 // post review
 app.post('books/:id/reviews', (req, res) => {
-  const id = req.params.id;
-  // grab review text
-  // grab review rating
-  db.postReview(id, review, rating)
+  let id = req.params.id;
+  let review = req.body.review;
+  let rating = req.body.rating;
+  db.postReview(id, review, rating).then(() => {
+    db.getReviews(id)
+  }).then(reviews => {
+    res.send(reviews)
+  })
 })
 
 
