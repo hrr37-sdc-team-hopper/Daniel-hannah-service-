bc3e92fe96b460e822953547b2c4789307c8fdaa
Hannah Kim <hannahsohkim@gmail.com>
2019-04-01 23:39:00 -0700

    Apply airbnb lint

--- db/index.js
+++ db/index.js
@@ -1,56 +1,57 @@
 const mysql = require('mysql');
 const config = require('./config.js');
+
 const connection = mysql.createConnection(config);
 
-connection.connect(err => {
+connection.connect((err) => {
   if (err) {
-    console.log(err, 'ERROR CONNECTING')
+    console.log(err, 'ERROR CONNECTING');
   } else {
-    console.log('connected')
+    console.log('connected');
   }
 });
 
-const insertUser  = (username) => {
+const insertUser = (username) => {
   return new Promise((resolve, reject) => {
-    let sql = 'INSERT INTO users (username) VALUES (?)';
+    const sql = 'INSERT INTO users (username) VALUES (?)';
     connection.query(sql, username, (err, result) => {
-      if (err) { reject(err) }
+      if (err) { reject(err); }
       resolve(result);
-    })
-  })
-}
+    });
+  });
+};
 
 const insertReview = (date, review, rating, book_id, user_id) => {
   return new Promise((resolve, reject) => {
-    let sql = 'insert into reviews (date, review, rating, book_id, user_id) values (?, ?, ?, ?, ?)';
-    let params = [date, review, rating, book_id, user_id];
+    const sql = 'insert into reviews (date, review, rating, book_id, user_id) values (?, ?, ?, ?, ?)';
+    const params = [date, review, rating, book_id, user_id];
     connection.query(sql, params, (err, result) => {
-      if (err) { reject(err) }
+      if (err) { reject(err); }
       resolve(result);
-    })
-  })
-}
+    });
+  });
+};
 
 const getReviews = (id) => {
   return new Promise((resolve, reject) => {
     const sql = 'select * from reviews where id = ?';
     connection.query(sql, id, (err, result) => {
-      if (err) { reject (err) }
+      if (err) { reject(err); }
       resolve(result);
-    })
-  })
-}
+    });
+  });
+};
 
 const getRatedReviews = (id, rating) => {
   return new Promise((resolve, reject) => {
     const sql = 'select * from reviews where (id = ?, rating = ?)';
-    let params = [id, rating];
+    const params = [id, rating];
     connection.query(sql, params, (err, result) => {
-      if (err) { reject (err) }
+      if (err) { reject(err); }
       resolve(result);
     });
-  })
-}
+  });
+};
 
 
 const postReview = (id, review, rating) => {
@@ -58,11 +59,11 @@ const postReview = (id, review, rating) => {
     const sql = 'insert into reviews (review, rating) values (?, ?) where id = ?';
     const params = [id, review, rating];
     connection.query(sql, params, (err, result) => {
-      if (err) { reject (err) }
+      if (err) { reject(err); }
       resolve(result);
-    })
-  })
-}
+    });
+  });
+};
 
 
 module.exports = {
@@ -72,4 +73,4 @@ module.exports = {
   getRatedReviews,
   postReview,
   connection
-}
+};
