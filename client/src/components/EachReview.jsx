53bec440bd3e0dfe501eb0058c80c46478777e85
Hannah Kim <hannahsohkim@gmail.com>
2019-04-06 00:35:58 -0700

    Destructure props object and refactor inline styling to styled component

--- client/src/components/EachReview.jsx
+++ client/src/components/EachReview.jsx
@@ -30,46 +30,39 @@ const Image = styled.a`
   float: left;
 `;
 
-class EachReview extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      rating: ''
-    };
-  }
+const Float = styled.span`
+  float: right;
+`;
 
-  render() {
-    const float = {
-      float: 'right',
-    };
+const EachReview = (props) => {
+  const { userId, users, rating, date, review } = props;
 
-    return (
-      <div>
-        {this.props.users.map((user, index) => {
-          if (user.id === this.props.userId) {
-            return (
-              <div key={index}>
-                <Image>
-                  <img src={user.avatar} alt="" />
-                </Image>
-                <UserDetails>
-                  <User>
-                    {user.username}
-                  </User>
-                  <span> rated it </span>
-                  <StarRatings rating={this.props.rating} starRatedColor="#FF7F50" numberOfStars={5} name="rating" starDimension="20px" starSpacing="0px" />
-                  <span style={float}>{this.props.date}</span>
-                  <ReviewContent>
-                    <p>{this.props.review}</p>
-                  </ReviewContent>
-                </UserDetails>
-              </div>
-            );
-          }
-        })}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      {users.map((user, index) => {
+        if (user.id === userId) {
+          return (
+            <div key={index}>
+              <Image>
+                <img src={user.avatar} alt="" />
+              </Image>
+              <UserDetails>
+                <User>
+                  {user.username}
+                </User>
+                <span> rated it </span>
+                <StarRatings rating={rating} starRatedColor="#FF7F50" numberOfStars={5} name="rating" starDimension="20px" starSpacing="0px" />
+                <Float>{date}</Float>
+                <ReviewContent>
+                  <p>{review}</p>
+                </ReviewContent>
+              </UserDetails>
+            </div>
+          );
+        }
+      })}
+    </div>
+  );
+};
 
 export default EachReview;
